57d94dc86faafc2944ed487a77a5e1e538a2ec7d
Eric Nguyen <eric.duy@gmail.com>
2015-10-09 23:12:06 -0700

    mongo added, using collection name main. Data will be stored at data folder

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongo://localhost:27017/main';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  
 });
 
 module.exports = router;
